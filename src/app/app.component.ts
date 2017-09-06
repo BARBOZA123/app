26790516d9fd54f35244b2d91a0c41e8a1185706
Armando Barboza Ojeda <1411730@SOAM.TCS.com>
2017-09-06 17:48:34 -0500

    new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }    from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { routing }        from './app.routing';
+ 
+import { AuthGuard } from './_guards/auth.guard';
+import { UserService } from './_services/user.service';
+import { AuthenticationService } from './_services/authentication.service';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './_login/login.component';
+
 
 @NgModule({
+  
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
+
   imports: [
-    BrowserModule
+  	NgbModule.forRoot(),
+    BrowserModule,
+    routing,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  
+  providers: [
+    AuthGuard,
+    AuthenticationService,
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
